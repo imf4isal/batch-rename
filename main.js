a9975e65f6af156056b93e54373105a31e8aa19b
Faisal <mdfaisal.dev@gmail.com>
2025-03-19 07:19:58 +0600

    parsing argument

---
+++ main.js
@@ -0,0 +1,25 @@
+function parseArgs() {
+  const args = process.argv.slice(2);
+
+  if (args.length < 3) {
+    console.error(
+      "Usage: node rename-files.js <folderPath> <currentExtension> <newExtension>"
+    );
+    console.error("Example: node rename-files.js ./my_subtitles web_vtt srt");
+    process.exit(1);
+  }
+
+  const folderPath = args[0];
+  const currentExtension = args[1].startsWith(".") ? args[1] : `.${args[1]}`;
+  const newExtension = args[2].startsWith(".") ? args[2] : `.${args[2]}`;
+
+  return { folderPath, currentExtension, newExtension };
+}
+
+function main() {
+  const args = parseArgs();
+
+  console.log(args);
+}
+
+main();
